31ae1b083f8b6f7a7fd0d5226f629785ca07fb10
OliverOstman <oliver.ostman@metropolia.fi>
2019-03-20 16:10:36 +0200

    update

---
+++ src/components/tbody.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tr from './tr';
+import PropTypes from 'prop-types';
+
+const Tbody = (props) => {
+    return (
+        <Tr/>
+    );
+};
+
+Tbody.propTypes = {
+    picArray: PropTypes.array,
+};
+
+export default Tbody;
---
+++ src/components/tr.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Tr = (props) => {
+    return (
+        <h1>Tr</h1>
+    );
+};
+
+Tr.propTypes = {
+    picArray: PropTypes.array,
+};
+
+export default Tr;
